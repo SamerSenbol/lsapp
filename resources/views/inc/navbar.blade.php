b9cf67386a0989feab00d637e2691c2df5112865
SamerSenbol <samer.senbol@gmail.com>
2019-09-07 21:00:58 +0200

    Adding the app page names in the navbar href.

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -6,13 +6,13 @@
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+        <li class="nav-item active">
+            <a class="nav-link" href="/about">About<span class="sr-only">(current)</span></a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        <li class="nav-item active">
+            <a class="nav-link" href="/services">Servicse<span class="sr-only">(current)</span></a>
         </li>
         </ul>
     </div>
