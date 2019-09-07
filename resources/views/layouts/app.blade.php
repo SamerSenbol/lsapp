21af04a3dbd8df6c80e4c2a3b3b00ef9981ac0a6
SamerSenbol <samer.senbol@gmail.com>
2019-09-07 18:12:00 +0200

    Adding the css link from the asset in the public folder.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel= "stylesheet" href="{{asset('css/app.css')}}">
         <title>{{config('app.name','Lsapp')}}</title>
     </head>
     <body>
