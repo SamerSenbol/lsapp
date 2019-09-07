01d107a8b12d1e8d4fc5b5a86ea6607c0c613798
SamerSenbol <samer.senbol@gmail.com>
2019-09-07 17:04:18 +0200

    Passing a single value by using the method: ->with(key,value);

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,11 +7,13 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index(){
-        $title = 'Welcome to Laravel';
-        return view ('pages.index',compact('title'));
+        $title = 'Welcome to Laravel!';
+        //return view ('pages.index',compact('title'));
+        return view ('pages.index')->with('title', $title);
     }
     public function about(){
-        return view ('pages.about');
+        $title = 'About us';
+        return view ('pages.about')->with('title', $title);
     }
     public function services(){
         return view ('pages.services');
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>About</h1>
+    <h1><?php echo $title ?></h1>
     <p>This is the about page</p>
 @endsection
