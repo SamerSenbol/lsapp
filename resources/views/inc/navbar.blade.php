a87e0319e2df997a852c724a7db320521ab1a9c9
SamerSenbol <samer.senbol@gmail.com>
2019-09-12 12:57:48 +0200

    Adding to navbar list the Blog item in the navbar.blade.php.

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -18,6 +18,9 @@
         <li class="nav-item active">
             <a class="nav-link" href="/services">Servicse<span class="sr-only">(current)</span></a>
         </li>
+        <li class="nav-item active">
+            <a class="nav-link" href="/posts">Blog<span class="sr-only">(current)</span></a>
+        </li>
         </ul>
     </div>
 </nav>
