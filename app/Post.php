8fad4b4f89a8d10b3dca6c81b656b32317006f0f
SamerSenbol <samer.senbol@gmail.com>
2019-09-12 11:57:04 +0200

    Changing some of the default model properties.

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    // Table Name
+    protected $table = 'posts';
+    // Primary key field
+    public $primaryKey = 'id';
+    //Timestamps
+    public $timestamps = true;
 }
