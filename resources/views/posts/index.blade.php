20600e3f5f2a5df8fd7ffe1adfeed56e5895cbc5
SamerSenbol <samer.senbol@gmail.com>
2019-09-12 12:50:53 +0200

    Looping through the Post model in the index.blade.php file to view all the posts that we have in the database.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.app')
 @section('content')
     <h1>Posts</h1>
-    <h1>Posts</h1>
-    <h1>Posts</h1>
+    
+    @if(count($posts) > 1)
+        @foreach ($posts as $post)
+            <div class="well">
+                <h3>{{$post->title}}</h3>
+                <small>written on{{$post->created_at}}</small>
+            </div>
+        @endforeach
+    @else 
+        <p>No posts found</p>
+    @endif
+
 @endsection
