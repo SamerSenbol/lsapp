b1958ee6390da58a850dd5fe08f2a0e96fbf4c14
SamerSenbol <samer.senbol@gmail.com>
2019-09-13 16:27:57 +0200

    Putting the post's title via anchor tag then passing the post Id to the href by: {{$post->id}}.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,7 @@
     @if(count($posts) > 1)
         @foreach ($posts as $post)
             <div class="well">
-                <h3>{{$post->title}}</h3>
+            <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
                 <small>written on{{$post->created_at}}</small>
             </div>
         @endforeach
