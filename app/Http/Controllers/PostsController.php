71850bbff681a8bdd3d92efd6570ad6c61031811
SamerSenbol <samer.senbol@gmail.com>
2019-09-13 22:54:31 +0200

    Using orderby() instead of all() model method.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,7 +14,7 @@ class PostsController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::orderby('title','desc')->get();
         return view('posts.index')->with('posts' , $posts);
     }
 
