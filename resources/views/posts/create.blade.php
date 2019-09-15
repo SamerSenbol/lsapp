4db6b7a01b09d6153903d1d180846184b70a9e15
SamerSenbol <samer.senbol@gmail.com>
2019-09-15 15:13:13 +0200

    Instead of using URL we are going to use actions array and set that to controller functions which is here: PostsController@store and we set the POST method also. And then we start to add the forms group that we need.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,7 +1,15 @@
 @extends('layouts.app')
 @section('content')
     <h1>Create Posts</h1>
-    {!! Form::open(['url' => 'foo/bar']) !!}
-    //
+    {!! Form::open(['action'=>'PostsController@store','method'=>'POST']) !!}
+        <div class="form-group">
+            {{Form::label('title','Title')}}
+            {{Form::text('title','',['class'=> 'form-control','placeholder'=>'Title'])}}
+        </div>
+        <div class="form-group">
+            {{Form::label('body','Body')}}
+            {{Form::textarea('body','',['class'=> 'form-control','placeholder'=>'Body'])}}
+        </div>
+        {{Form::submit('submit',['class'=>'btn btn-primary'])}}
     {!! Form::close() !!}
 @endsection
