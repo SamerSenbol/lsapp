ca20d556c083adce3e7b7ae50fab605c0a4b4d67
SamerSenbol <samer.senbol@gmail.com>
2019-09-20 14:27:45 +0200

    We already have the users table so for making login system we just need add this commit to enable it: php artisan make:auth.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ Route::get('/about', 'PagesController@about');
 Route::get('/services', 'PagesController@services');
 
 Route::resource('posts','PostsController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
