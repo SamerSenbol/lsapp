0eb791cddf569588443857e617440c3ed376c8ff
SamerSenbol <samer.senbol@gmail.com>
2019-09-20 13:24:53 +0200

    For the delete we cannot just add anchor tag to delete but we should create a POST request via form and then spoof it with delete.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,6 +7,10 @@
     </div>
     <hr>
     <small>Written on {{$post->created_at}}</small>
-<a href="/posts/{{$post->id}}/edit" class ="btn btn-default">Edit</a>
+    <a href="/posts/{{$post->id}}/edit" class ="btn btn-default">Edit</a>
 
+    {!!Form::open(['action'=>['PostsController@destroy',$post->id],'method'=> 'POST','class' => 'pull-right'])!!}
+        {{Form::hidden('_method','DELETE')}}
+        {{Form::submit('Delete',['class'=> 'btn btn-danger'])}}
+    {!!Form::close()!!}
 @endsection
