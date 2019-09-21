7cf048cd300d36f51e6aa77525a02308a47524cc
SamerSenbol <samer.senbol@gmail.com>
2019-09-21 10:03:03 +0200

    Changing the routing for the home to dashboard(because it's something regarding with backend).

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/services', 'PagesController@services');
 Route::resource('posts','PostsController');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'DashboardController@index');
