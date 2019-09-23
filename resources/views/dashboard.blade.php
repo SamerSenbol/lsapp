a794d0963591ca5d6bda50c050667096165d805e
SamerSenbol <samer.senbol@gmail.com>
2019-09-23 09:12:56 +0200

    Copy paste the delete form from the show.blade.php to dashboard.blade.php.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -27,7 +27,12 @@
                                 <tr>
                                 <td>{{$post->title}}</td>
                                 <td><a href="/posts/{{$post->id}}/edit" class = "btn btn-default">Edit</a></td>
-                                    <td></td>
+                                    <td>
+                                        {!!Form::open(['action'=>['PostsController@destroy',$post->id],'method'=> 'POST','class' => 'pull-right'])!!}
+                                        {{Form::hidden('_method','DELETE')}}
+                                        {{Form::submit('Delete',['class'=> 'btn btn-danger'])}}
+                                        {!!Form::close()!!}
+                                    </td>
                                 </tr>
                             @endforeach
                         </table>
