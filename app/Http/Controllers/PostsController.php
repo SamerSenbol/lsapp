016e40059d63782dd74f018d6a980c1ce04ee835
SamerSenbol <samer.senbol@gmail.com>
2019-09-23 09:58:52 +0200

    We need to implement the access controller for authentication to prevent the guests to create post.
    In the DashboardController we have a constructor which is a function its run when the class is instantiated to block everthing if the user is not ethenticated.
    
    So we copy it to the PostsController.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,6 +8,16 @@ use App\Post;
 class PostsController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
