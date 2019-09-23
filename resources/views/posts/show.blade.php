ef4723705be31eb90c39d3c354a09c266a59f77c
SamerSenbol <samer.senbol@gmail.com>
2019-09-23 11:16:14 +0200

    We should hide the edit and delete buttons for the guests who is  not logged in and they still can see the blog.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,10 +7,12 @@
     </div>
     <hr>
 <small>Written on {{$post->created_at}} by {{$post->user->name}} </small>
-    <a href="/posts/{{$post->id}}/edit" class ="btn btn-default">Edit</a>
-
-    {!!Form::open(['action'=>['PostsController@destroy',$post->id],'method'=> 'POST','class' => 'pull-right'])!!}
-        {{Form::hidden('_method','DELETE')}}
-        {{Form::submit('Delete',['class'=> 'btn btn-danger'])}}
-    {!!Form::close()!!}
+    @if (!Auth::guest())
+        <a href="/posts/{{$post->id}}/edit" class ="btn btn-default">Edit</a>
+        
+        {!!Form::open(['action'=>['PostsController@destroy',$post->id],'method'=> 'POST','class' => 'pull-right'])!!}
+            {{Form::hidden('_method','DELETE')}}
+            {{Form::submit('Delete',['class'=> 'btn btn-danger'])}}
+        {!!Form::close()!!}
+    @endif
 @endsection
