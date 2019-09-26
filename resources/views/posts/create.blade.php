5fa0b09b89eac8606c63c16e6f1562e86dc65e5a
SamerSenbol <samer.senbol@gmail.com>
2019-09-26 12:58:39 +0200

    Adding file upload button in create.blade.php file by creating form-group div and adding enctype attribute which called multipart/data in the main form.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <h1>Create Posts</h1>
-    {!! Form::open(['action'=>'PostsController@store','method'=>'POST']) !!}
+    {!! Form::open(['action'=>'PostsController@store','method'=>'POST','enctype'=>'multipart/data']) !!}
         <div class="form-group">
             {{Form::label('title','Title')}}
             {{Form::text('title','',['class'=> 'form-control','placeholder'=>'Title'])}}
@@ -9,6 +9,10 @@
         <div class="form-group">
             {{Form::label('body','Body')}}
             {{Form::textarea('body','',[ 'id'=> 'article-ckeditor','class'=> 'form-control','placeholder'=>'Body'])}}
+        </div>
+        <div class="form-group">
+            {{Form::file('cover_image')}}
+            
         </div>
         {{Form::submit('submit',['class'=>'btn btn-primary'])}}
     {!! Form::close() !!}
